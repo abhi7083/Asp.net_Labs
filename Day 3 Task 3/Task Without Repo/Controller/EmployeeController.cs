ad447cb5ac1f1ca86fa73b8ac7f45571eb234a63
abhi7083 <71085654+abhi7083@users.noreply.github.com>
2022-07-28 11:13:29 +0530

    Add files via upload

---
+++ Day 3 Task 3/Task Without Repo/Controller/EmployeeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDbContext _context;
+
+        public EmployeeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> stList = _context.emps.ToList();
+            return View(stList);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            _context.emps.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee obj = _context.emps.Find(id);
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Employee obj = _context.emps.Find(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee obj)
+        {
+            _context.emps.Update(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Employee obj = _context.emps.Find(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult Deleteconfirm(int id)
+        {
+            Employee obj = _context.emps.Find(id);
+            _context.emps.Remove(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
